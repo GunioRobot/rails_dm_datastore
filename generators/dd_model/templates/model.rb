c26579a288b25222e349de4d3f1680dd73fecd4f
Gun.io Whitespace Robot <contact@gun.io>
2011-10-30 04:30:35 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- generators/dd_model/templates/model.rb
+++ generators/dd_model/templates/model.rb
@@ -2,7 +2,7 @@ class <%= class_name %>
 <% max = 2
   presets = {'Text' => ':lazy => false', 'String' => ':length => 500'}
   reserved_dm_names = DataMapper::Resource.instance_methods +
-      DataMapper::Resource.private_instance_methods 
+      DataMapper::Resource.private_instance_methods
   Array(attributes).each do |attribute|
     if reserved_dm_names.include? attribute.name
       raise "reserved property name '#{attribute.name}'"
@@ -13,7 +13,7 @@ class <%= class_name %>
   end -%>
   include DataMapper::Resource
   storage_names[:default] = "<%= class_name %>"
-  
+
   property :id,<%= " " * (max - 2) %> Serial
 <% Array(attributes).each do |attribute|
      klass = attribute.type.to_s.camelcase
@@ -24,6 +24,6 @@ class <%= class_name %>
      %><%= "#{klass}" %>, <%= " " * rad %>:required => true<%= more %>
 <% end -%>
 <% unless options[:skip_timestamps] -%>
-  timestamps :at 
+  timestamps :at
 <% end -%>
 end
--- generators/dd_model/templates/unit_test.rb
+++ generators/dd_model/templates/unit_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
- 
+
 class <%= class_name %>Test < ActiveSupport::TestCase
   # Replace this with your real tests.
   def test_truth
